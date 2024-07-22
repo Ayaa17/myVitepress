7a2b22d5c0d09b1b4e48ee975cfecdae618a6b4d
kent <kent831217@gmail.com>
2024-07-22 15:43:41 +0800

    update layout

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -11,16 +11,26 @@ export default sidebar({
     //   children: "structure",
     // },
     {
-      text: "Articles",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
+      text: "AI",
+      icon: "lightbulb",
+      prefix: "ai/",
+      children: [
+        "ml knowledge/",
+        "whisper/",
+        "yolo/",
+        "application/"
+      ],
     },
-    "intro",
+    // "intro",
     // {
     //   text: "Slides",
     //   icon: "person-chalkboard",
     //   link: "https://plugin-md-enhance.vuejs.press/guide/content/revealjs/demo.html",
     // },
   ],
+
+  "/others/": "structure",
+
+  "/ai/": "structure",
+
 });
